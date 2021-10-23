b75924053a967d79e6930d331a7ce1306493c876
HaPPyeR29 <happyer.present@gmail.com>
2021-10-23 19:31:44 +0300

    new life

--- anim/anim.js
+++
@@ -1,8 +0,0 @@
-gsap.to("#gear", {duration: 1, rotation: 360, transformOrigin: "center center"})
-gsap.to(".line1", {duration: 1.2, rotation: 360, transformOrigin: "top left"});
-gsap.to(".line2", {duration: 1.2, rotation: 360, transformOrigin: "bottom right"});
-gsap.to(".infinity1", {duration: 10, repeat: -1, paused: false, ease: "none", rotation: 360, transformOrigin: "370px 232px"})
-gsap.to(".infinity2", {duration: 10, repeat: -1, paused: false, ease: "none", rotation: 360, transformOrigin: "155px 352px"})
-
-
-
