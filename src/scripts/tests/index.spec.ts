d64647c3a2af0dd64cea4843d5191aa5c4c2c3a5
happyer29 <happyer.present@gmail.com>
2021-10-31 13:10:38 +0300

    added ts-ignore

--- src/scripts/tests/index.spec.ts
+++ src/scripts/tests/index.spec.ts
@@ -1,6 +1,8 @@
 //index.spec не трогаем, тут просто скрипт который пробегается
 //по всей папке tests и собирает все файлы .spec.ts воедино
+// @ts-ignore
 context = require.context('./', true, /\.spec\.ts$/);
+// @ts-ignore
 context.keys().forEach(context);
 module.exports = context;
 
