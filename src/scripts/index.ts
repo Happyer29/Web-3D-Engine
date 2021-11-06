33cd36e1e0329bd2b4546fdfe6bcdedfce2efd91
happyer29 <happyer.present@gmail.com>
2021-11-06 16:06:04 +0300

    webgl renderer class init + setsize method

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,23 @@
 import {Matrix3} from "./W3DE/maths/Matrix3";
+import {WebGLRenderer} from "./W3DE/renderer/WebGLRenderer";
 
-let t = new Matrix3([[0,1,0]]);
+let gl = new WebGLRenderer();
 
-console.log(t.matrix)
+gl.setSize(500, 500, false);
+
+let ctx = gl.getCtx();
+ctx.lineWidth   = 1;
+ctx.strokeStyle = '#f00';
+ctx.fillStyle   = '#eff';
+
+ctx.fillRect(  10.5, 10.5, 20, 20 );
+ctx.strokeRect( 10.5, 10.5, 20, 20 );
+ctx.fillRect(   40, 10.5, 20, 20 );
+ctx.strokeRect( 40, 10.5, 20, 20 );
+ctx.fillRect(   70, 10, 20, 20 );
+ctx.strokeRect( 70, 10, 20, 20 );
+
+
 
 
 
