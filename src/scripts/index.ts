e20152caa854547fb8e50148e8dcad498f450a58
Nikita <32851787+Happyer29@users.noreply.github.com>
2021-11-10 16:35:42 +0300

    Revert "webglrenderer class trying to add"

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,23 +1,8 @@
 import {Matrix3} from "./W3DE/maths/Matrix3";
-import {WebGLRenderer} from "./W3DE/renderer/WebGLRenderer";
 
-let gl = new WebGLRenderer();
+let t = new Matrix3([[0,1,0]]);
 
-gl.setSize(500, 500, false);
-
-let ctx = gl.getCtx();
-ctx.lineWidth   = 1;
-ctx.strokeStyle = '#f00';
-ctx.fillStyle   = '#eff';
-
-ctx.fillRect(  10.5, 10.5, 20, 20 );
-ctx.strokeRect( 10.5, 10.5, 20, 20 );
-ctx.fillRect(   40, 10.5, 20, 20 );
-ctx.strokeRect( 40, 10.5, 20, 20 );
-ctx.fillRect(   70, 10, 20, 20 );
-ctx.strokeRect( 70, 10, 20, 20 );
-
-
+console.log(t.matrix)
 
 
 
