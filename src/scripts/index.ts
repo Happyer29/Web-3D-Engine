8b463f47c271853254926e9b65a8b884f82ccbd8
arsenijlevin <arsenij.levin19@gmail.com>
2021-11-21 12:04:19 +0300

    compose exports to W3DE

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,13 +1,6 @@
-import {Matrix3} from "./W3DE/maths/Matrix3";
-import {ObjectParser} from "./W3DE/obj-parser/ObjectParser";
-import {FileLoader} from "./W3DE/loaders/FileLoader";
-import {TextureLoader} from "./W3DE/loaders/TextureLoader";
-import {Material} from "./W3DE/materials/Material";
-import {WebGLRenderer} from "./W3DE/renderer/WebGLRenderer";
-import { SphereGeometry } from "./W3DE/geometries/SphereGeometry";
-import { Mesh } from "./W3DE/objects/Mesh";
+import * as W3DE from "./W3DE/W3DE";
 
-let t = new Matrix3([[0, 1, 0]]);
+let t = new W3DE.Matrix3([[0, 1, 0]]);
 
 console.log(t.matrix)
 
@@ -21,8 +14,6 @@ let buttons : HTMLButtonElement[] = [
     drawGeometryBtn,
 ]
 
-
-
 objInput.addEventListener('change', readObjectFromInput, false);
 textureInput.addEventListener('change', readTextureFromInput, false);
 drawGeometryBtn.addEventListener('click', drawGeometry, false);
@@ -32,10 +23,10 @@ async function readObjectFromInput(event : Event) {
 
     let files = input.files;
     let file = files[0];
-    let fileText = await FileLoader.loadAsText(file);
-    let object = await new ObjectParser().parseObjectFromString(fileText);
+    let fileText = await W3DE.FileLoader.loadAsText(file);
+    let object = await new W3DE.ObjectParser().parseObjectFromString(fileText);
 
-    let renderer = new WebGLRenderer(object, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
+    let renderer = new W3DE.WebGLRenderer(object, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
     renderer.resizeCanvasToDisplaySize();
     renderer.render();
     console.log(object)
@@ -51,8 +42,8 @@ async function readTextureFromInput(event : Event) {
     let files = input.files;
     let file = files[0];
 
-    let texture = await TextureLoader.loadFromFile(file);
-    let material = new Material(texture);
+    let texture = await W3DE.TextureLoader.loadFromFile(file);
+    let material = new W3DE.Material(texture);
     console.log(material);
 
     buttons.forEach(button => {
@@ -61,10 +52,10 @@ async function readTextureFromInput(event : Event) {
 }
 
 async function drawGeometry() {
-    const sphereGeometry = new SphereGeometry(50, 100); // change roundness to 10-20 to clearly see rotation
-    const defaultMaterial = await Material.getDefaultMaterial();
-    const mesh = new Mesh(sphereGeometry, defaultMaterial);
-    let renderer = new WebGLRenderer(mesh, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
+    const sphereGeometry = new W3DE.SphereGeometry(50, 100); // change roundness to 10-20 to clearly see rotation
+    const defaultMaterial = await W3DE.Material.getDefaultMaterial();
+    const mesh = new W3DE.Mesh(sphereGeometry, defaultMaterial);
+    let renderer = new W3DE.WebGLRenderer(mesh, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
     renderer.resizeCanvasToDisplaySize();
     renderer.render();
 
