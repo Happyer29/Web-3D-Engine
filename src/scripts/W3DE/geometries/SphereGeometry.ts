87a0f889279d36bdff6c5c79999cec5b857ff5a6
arsenijlevin <arsenij.levin19@gmail.com>
2021-11-21 11:56:10 +0300

    adds button in html to render SphereGeometry

--- src/scripts/W3DE/geometries/SphereGeometry.ts
+++ src/scripts/W3DE/geometries/SphereGeometry.ts
@@ -1,62 +1,51 @@
-import { Object3D } from "../core/Object3D";
-import { Material } from "../materials/Material";
-import { Vector3 } from "../maths/Vector3";
 import { Geometry } from "./Geometry";
 
 export class SphereGeometry extends Geometry {
-  constructor(radius: number = 1,
-    subdivisionsAxis = 32,
-    subdivisionsHeight = 16,
-    opt_startLatitudeInRadians?: number,
-    opt_endLatitudeInRadians?: number,
-    opt_startLongitudeInRadians?: number,
-    opt_endLongitudeInRadians?: number) {
+    constructor(radius: number, roundness: number = 100) {
 
-    opt_startLatitudeInRadians = opt_startLatitudeInRadians || 0;
-    opt_endLatitudeInRadians = opt_endLatitudeInRadians || Math.PI;
-    opt_startLongitudeInRadians = opt_startLongitudeInRadians || 0;
-    opt_endLongitudeInRadians = opt_endLongitudeInRadians || (Math.PI * 2);
+        if (radius < 0) throw new Error("Radius must be greater than 0");
+        if (roundness < 1 || roundness > 100) throw new Error("Roundness must be in range [10; 100]");
 
+        const width = roundness % 2 == 0 ? roundness : roundness + 1;
+        const height = width / 2;
 
-    const latRange = opt_endLatitudeInRadians - opt_startLatitudeInRadians;
-    const longRange = opt_endLongitudeInRadians - opt_startLongitudeInRadians;
+        const positions = [];
+        const normals = [];
+        const texCoords = [];
 
-    const positions = [];
-    const normals = [];
-    const texCoords = [];
-   
-    function addPoint(x, y) {
-      const u = x / subdivisionsAxis;
-      const v = y / subdivisionsHeight;
-      const theta = longRange * u + opt_startLongitudeInRadians;
-      const phi = latRange * v + opt_startLatitudeInRadians;
-      const sinTheta = Math.sin(theta);
-      const cosTheta = Math.cos(theta);
-      const sinPhi = Math.sin(phi);
-      const cosPhi = Math.cos(phi);
-      const ux = cosTheta * sinPhi;
-      const uy = cosPhi;
-      const uz = sinTheta * sinPhi;
-      positions.push(radius * ux, radius * uy, radius * uz);
-      normals.push(ux, uy, uz);
-      texCoords.push(1 - u, v);
+        function addPoint(x: number, y: number) {
+            const u = x / width;
+            const v = y / height;
+
+            const sin2PIu = Math.sin(2 * Math.PI * u);
+            const cos2PIu = Math.cos(2 * Math.PI * u);
+            const sinPIv = Math.sin( Math.PI * v);
+            const cosPIv = Math.cos( Math.PI * v);
+
+            const ux = cos2PIu * sinPIv;
+            const uy = cosPIv;
+            const uz = sin2PIu * sinPIv;
+
+            positions.push(radius * ux, radius * uy, radius * uz);
+            normals.push(ux, uy, uz);
+            texCoords.push(1 - u, v);
+        }
+
+        for (let y = 0; y <= height; y++) {
+            for (let x = 0; x <= width; x++) {
+                // for each rect we need 6 points
+                addPoint(x, y);
+                addPoint(x + 1, y);
+                addPoint(x, y + 1);
+
+                addPoint(x, y + 1,);
+                addPoint(x + 1, y);
+                addPoint(x + 1, y + 1);
+            }
+        }
+
+        // build geometry
+        super(positions, normals, texCoords)
+        this.type = 'SphereGeometry';
     }
-
-    for (let y = 0; y <= subdivisionsHeight; y++) {
-      for (let x = 0; x <= subdivisionsAxis; x++) {
-        // for each rect we need 6 points
-        addPoint(x, y);
-        addPoint(x + 1, y);
-        addPoint(x, y + 1);
-
-        addPoint(x, y + 1,);
-        addPoint(x + 1, y);
-        addPoint(x + 1, y + 1);
-      }
-    }
-
-    // build geometry
-    super(positions, normals, texCoords)
-    this.type = 'SphereGeometry';
-  }
 }
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -11,45 +11,23 @@ let t = new Matrix3([[0, 1, 0]]);
 
 console.log(t.matrix)
 
-let objInput = document.querySelector("#obj-loader");
-let textureInput = document.querySelector("#texture-loader");
+let objInput : HTMLButtonElement = document.querySelector("#obj-loader");
+let textureInput : HTMLButtonElement = document.querySelector("#texture-loader");
+let drawGeometryBtn : HTMLButtonElement = document.querySelector("#draw-geometry");
+
+let buttons : HTMLButtonElement[] = [
+    objInput,
+    textureInput,
+    drawGeometryBtn,
+]
+
 
 
 objInput.addEventListener('change', readObjectFromInput, false);
-
-
 textureInput.addEventListener('change', readTextureFromInput, false);
+drawGeometryBtn.addEventListener('click', drawGeometry, false);
 
 async function readObjectFromInput(event : Event) {
-    // const input = event.target as HTMLInputElement;
-
-    // let files = input.files;
-    // let file = files[0];
-    // let fileText = await FileLoader.loadAsText(file);
-    // let object = await new ObjectParser().parseObjectFromString(fileText);
-
-    // let renderer = new WebGLRenderer(object, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
-    // renderer.resizeCanvasToDisplaySize();
-    // renderer.render();
-    // console.log(object)
-
-    const sphereGeometry = new SphereGeometry(50, 128, 64);
-    const defaultMaterial = await Material.getDefaultMaterial();
-    const mesh = new Mesh(sphereGeometry, defaultMaterial);
-    let renderer = new WebGLRenderer(mesh, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
-    renderer.resizeCanvasToDisplaySize();
-    renderer.render();
-}
-
-async function readTextureFromInput(event : Event) {
-    // const input = event.target as HTMLInputElement;
-    
-    // let files = input.files;
-    // let file = files[0];
-
-    // let texture = await TextureLoader.loadFromFile(file);
-    // let material = new Material(texture);
-    // console.log(material);
     const input = event.target as HTMLInputElement;
 
     let files = input.files;
@@ -60,6 +38,37 @@ async function readTextureFromInput(event : Event) {
     let renderer = new WebGLRenderer(object, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
     renderer.resizeCanvasToDisplaySize();
     renderer.render();
+    console.log(object)
 
+    buttons.forEach(button => {
+        button.disabled = true;
+    })    
 }
 
+async function readTextureFromInput(event : Event) {
+    const input = event.target as HTMLInputElement;
+    
+    let files = input.files;
+    let file = files[0];
+
+    let texture = await TextureLoader.loadFromFile(file);
+    let material = new Material(texture);
+    console.log(material);
+
+    buttons.forEach(button => {
+        button.disabled = true;
+    }) 
+}
+
+async function drawGeometry() {
+    const sphereGeometry = new SphereGeometry(50, 100); // change roundness to 10-20 to clearly see rotation
+    const defaultMaterial = await Material.getDefaultMaterial();
+    const mesh = new Mesh(sphereGeometry, defaultMaterial);
+    let renderer = new WebGLRenderer(mesh, {selector: "#canvas-parent", width: "1000px", height: "1000px"});
+    renderer.resizeCanvasToDisplaySize();
+    renderer.render();
+
+    buttons.forEach(button => {
+        button.disabled = true;
+    }) 
+}
