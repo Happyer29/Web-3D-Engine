b1d44c6d0e04ca88c79ccaa6cbd700a0412dfc21
happyer29 <happyer.present@gmail.com>
2021-11-24 08:11:05 +0300

    matrix 3 static utils

--- src/scripts/W3DE/maths/Matrix3.ts
+++ src/scripts/W3DE/maths/Matrix3.ts
@@ -1,14 +1,15 @@
 import {fixedLengthArray} from "../utils/fixedLengthArray";
 import {Vector3} from "./Vector3";
+import {Matrix3Utils} from "../utils/Matrix3Utils";
 
-type matrix3 = fixedLengthArray<fixedLengthArray<number, 3>, 3> // equal to [[0,0,0], [0,0,0], [0,0,0]]
+export type matrix3 = fixedLengthArray<fixedLengthArray<number, 3>, 3> // equal to [[0,0,0], [0,0,0], [0,0,0]]
 //TODO ЧТО МЫ ВОЗВРАЩАЕМ ОТ СТАТИЧЕСКИХ МЕТОДОВ? ЭКЗЕМПЛЯР КЛАССА ИЛИ МАССИВ ЗНАЧЕНИЙ?
 export class Matrix3 {
     private _matrix: matrix3;
-    private static readonly _dimension: number = 3;
+    private readonly _dimension: number = 3;
 
     constructor(m: number[][] | matrix3) {
-        this._matrix = Matrix3.matrixToMatrix3(m);
+        this._matrix = Matrix3Utils.matrixToMatrix3(m);
     }
 
     get matrix() {
@@ -16,81 +17,36 @@ export class Matrix3 {
     }
 
     public setMatrix(m:number[][] | matrix3) {
-        this._matrix = Matrix3.matrixToMatrix3(m);
+        this._matrix = Matrix3Utils.matrixToMatrix3(m);
+        return this;
     }
 
     get dimension() {
-        return Matrix3._dimension;
+        return this._dimension;
     }
 
-    public static zeroMatrix(): matrix3 {
-        return [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+    public zeroMatrix(){
+        return Matrix3Utils.zeroMatrix();
     }
 
-    private static buildMatrix(value: number): matrix3 {
-        let res: matrix3 = Matrix3.zeroMatrix();
-        for (let i = 0; i < this._dimension; i++) {
-            for (let j = 0; j < this._dimension; j++) {
-                res[i][j] = value;
-            }
-        }
-        return res;
+    public matrixToMatrix3(){
+        this._matrix = Matrix3Utils.matrixToMatrix3(this._matrix);
+        return this;
     }
 
-    public static matrixToMatrix3(m: number[][] | matrix3): matrix3 {
-        let res = Matrix3.zeroMatrix();
-        //console.log(res);
-        for (let i = 0; i < this._dimension; i++) {
-            for (let j = 0; j < this._dimension; j++) {
-                res[i][j] = m[i] === undefined || m[i][j] === undefined  ? 0 : m[i][j];
-            }
-        }
-        return res;
+    public identityMatrix() {
+        this._matrix = Matrix3Utils.identityMatrix();
+        return this;
     }
 
-    //TODO сделать лучше?? если можно
-    public static identityMatrix() {
-        let res = Matrix3.zeroMatrix();
-        for (let i = 0; i < this._dimension; i++) {
-            res[i][i] = 1;
-        }
-        return res;
+    public sumArray(a: Matrix3) {
+        this._matrix = Matrix3Utils.sumArray(this._matrix, a._matrix);
+        return this;
     }
 
-    public static sumArray(a: Matrix3, b: Matrix3) {
-        let m1 = a.matrix;
-        let m2 = b.matrix;
-
-        let res = Matrix3.zeroMatrix();
-        for (let i = 0; i < this._dimension; i++) {
-            for (let j = 0; j < this._dimension; j++) {
-                res[i][j] = (m1[i][j] || 0) + (typeof m2[i][j] === 'undefined' ? 0 : m2[i][j]);
-            }
-        }
-        return res;
-    }
-
-    public static vectorMultiplication(m: Matrix3, v: Vector3) {
-        let vPos = v.positionArr;
-
-        let res = Vector3.zeroVector();
-        for (let i = 0; i < this._dimension; i++) {
-            for (let j = 0; j < this._dimension; j++) {
-                res[i] += m._matrix[i][j] * vPos[j];
-            }
-        }
-        return res;
-    }
-
-    public static transponse(m: Matrix3) {
-        let arr = m._matrix;
-        for (let i = 0; i < this._dimension; i++) {
-            for (let j = 0; j < i; j++) {
-                let tmp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = tmp;
-            }
-        }
-        return new Matrix3(arr);
+    public transponse() {
+        this._matrix = Matrix3Utils.transponse(this._matrix);
+        return this;
     }
 }
