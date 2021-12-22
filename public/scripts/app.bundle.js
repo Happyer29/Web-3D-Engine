7e04f694b2953a39d3c03d743b51c1240b2757fe
happyer29 <happyer.present@gmail.com>
2021-12-22 22:54:04 +0300

    epd

---
+++ public/scripts/app.bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/index.ts":
+/*!******************************!*\
+  !*** ./src/scripts/index.ts ***!
+  \******************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: TypeScript emitted no output for D:\\code\\myWorkspace\\Web-3D-Engine2\\src\\scripts\\index.ts.\n    at makeSourceMapAndFinish (D:\\code\\myWorkspace\\Web-3D-Engine2\\node_modules\\ts-loader\\dist\\index.js:52:18)\n    at successLoader (D:\\code\\myWorkspace\\Web-3D-Engine2\\node_modules\\ts-loader\\dist\\index.js:39:5)\n    at Object.loader (D:\\code\\myWorkspace\\Web-3D-Engine2\\node_modules\\ts-loader\\dist\\index.js:22:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scripts/index.ts"]();
+/******/ 	
+/******/ })()
+;
